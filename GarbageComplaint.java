e54e1676ba8bfc2a62be1077e30261c15d6832a1
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:57:41 +0530

    Delete GarbageComplaint.java

--- GarbageComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class GarbageComplaint extends Complaint {
-    public GarbageComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.GARBAGE);
-    }
-}
