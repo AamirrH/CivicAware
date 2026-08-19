189cdcab3280c58b974c186a57bf840ad240c8fa
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:58:52 +0530

    Delete StreetlightComplaint.java

--- StreetlightComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class StreetlightComplaint extends Complaint {
-    public StreetlightComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.STREETLIGHT);
-    }
-}
