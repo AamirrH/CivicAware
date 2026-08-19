4e743043e416d4ea9c1e2080fbd40fe3d158be34
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:59:04 +0530

    Delete TrafficSignalComplaint.java

--- TrafficSignalComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class TrafficSignalComplaint extends Complaint {
-    public TrafficSignalComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.TRAFFIC_SIGNAL);
-    }
-}
