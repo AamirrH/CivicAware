1fc492f7407d6d1c5cb27c26c3e56eba15db0565
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:58:10 +0530

    Delete PotholeComplaint.java

--- PotholeComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class PotholeComplaint extends Complaint {
-    public PotholeComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.POTHOLE);
-    }
-}
