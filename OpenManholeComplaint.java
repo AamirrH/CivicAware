0b4f1849506d11d958ff8635afe4c6f464fb0a79
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:57:56 +0530

    Delete OpenManholeComplaint.java

--- OpenManholeComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class OpenManholeComplaint extends Complaint {
-    public OpenManholeComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.OPEN_MANHOLE);
-    }
-}
