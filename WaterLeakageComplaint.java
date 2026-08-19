defb42d40103ad00214a1f4f5c68c5467c5671e1
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:59:16 +0530

    Delete WaterLeakageComplaint.java

--- WaterLeakageComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class WaterLeakageComplaint extends Complaint {
-    public WaterLeakageComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.WATER_LEAKAGE);
-    }
-}
