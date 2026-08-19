7ad8cdb8a42b514a3475d0f61004410939830d6d
Aamir Hussain <aamirr.1704@gmail.com>
2026-08-19 19:58:38 +0530

    Delete RoadFloodingComplaint.java

--- RoadFloodingComplaint.java
+++
@@ -1,7 +0,0 @@
-package Factory;
-
-public class RoadFloodingComplaint extends Complaint {
-    public RoadFloodingComplaint(long id, String description, String location) {
-        super(id, description, location, ComplaintType.ROAD_FLOODING);
-    }
-}
